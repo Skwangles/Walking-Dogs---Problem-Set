62ce3b59c99098bba2f8486002299b7fd8ce4ac2
Skwangles <skwanglesavenue@gmail.com>
2020-10-09 00:03:08 +1300

    Project is completed.
    Now increments out from the original cats, and changes the path as the squares around the Cat squares get blocked.

--- Walking Dogs - Problem Set/Program.cs
+++ Walking Dogs - Problem Set/Program.cs
@@ -18,10 +18,10 @@ namespace Walking_Dogs___Problem_Set
             Console.WriteLine("How many Rows(y)");
             int y = int.Parse(Console.ReadLine());
             Console.WriteLine("");
-            Console.WriteLine("Where are the cats in x,y.");
+            Console.WriteLine("Where are the cats in x,y");
             Console.WriteLine("Type X when finished");
 
-
+            //Sleeping Cats
             Console.Write("Cat Location(x,y): ");
             string[] CatLoc = Console.ReadLine().Split(',');
             while (CatLoc[0] != "X")
@@ -29,21 +29,124 @@ namespace Walking_Dogs___Problem_Set
                 CatLocations.Add(CatLoc);
                 Console.Write("Cat Location(x,y): ");
                 CatLoc = Console.ReadLine().Split(',');
-                
-            }
-            //
-            // End of information setup. 
-            //
+
+            }//dequeues and adds to a list
 
 
+            //Creates Grids
             var Draw = new string[y * 2 + 1, x * 4 + 1];
             var Grid = new Square[y, x];
 
-            //
-            //creates replica grid, but with the corrosponding class instances
-            //
+            var myStack = new Queue<Square>();//For breadth first, just change to Queue
+            var visited = new HashSet<Square>();
+            int inc = 0;
+
             CreateGrid();
             Display();
+
+            while (PathFinder())
+            {
+                inc++;
+
+                visited.Clear();
+                Console.WriteLine("New Best Path: " + Grid[y - 1, x - 1].Path);
+                foreach (var item in Grid)
+                {
+                    item.Path = "";
+                }
+                foreach (var a in CatLocations)
+                {
+                    Grid[int.Parse(a[1]) - 1, int.Parse(a[0]) - 1].MakeCat(inc, Grid);
+                }
+            }
+
+            Console.WriteLine("With " + inc + " squares away from the cats, the way is blocked.");
+
+            //End
+
+
+            //
+            //Functions
+            //
+
+            bool PathFinder()
+            {
+
+
+                myStack.Enqueue(Grid[0, 0]);
+                var harReached = false;
+                while (myStack.Count > 0 && !harReached)
+                {
+                    harReached = FindPath();
+                }
+                return harReached;
+            }
+
+
+
+            /*
+            string MakePath()
+            {
+                string NewPath = "";
+                var PathSplit = Path.Split();
+
+                for (int i = 0; i < PathSplit.Length; i++)
+                {
+                    PathSplit[i] = PathSplit[i].Replace("(", "");
+                    PathSplit[i] = PathSplit[i].Replace(")", "");
+                }
+
+                for (int b = PathSplit.Length - 1; b > 0; b-= 1)
+                {
+                    var temporary = PathSplit[b].Split(",");
+                    var comparison = PathSplit[b - 1].Split(",");
+                    if (int.Parse(temporary[1]) == 0 && int.Parse(temporary[0]) == 0)
+                    {
+                        NewPath = "0,0 " + NewPath;
+                    }                    
+                    else if (Grid[int.Parse(temporary[1]), int.Parse(temporary[0])].isNeighbour(Grid[int.Parse(comparison[1]), int.Parse(comparison[0])], Grid))
+                    {
+                        NewPath = Grid[int.Parse(comparison[1]), int.Parse(comparison[0])].posX.ToString + "," + Grid[int.Parse(comparison[1]), int.Parse(comparison[0])].posY + " " + NewPath;
+                    }
+                    
+                }
+                return NewPath;
+            }
+            */
+            bool FindPath()
+            {
+                var temp = myStack.Dequeue();
+                if (temp.ToString() == "E")
+                {
+                    temp.Path += "(" + temp.posX + "," + temp.posY + ")";
+
+
+
+                    myStack.Clear();
+                    return true;
+                }
+
+                if (temp.isCat)
+                {
+                    return false;
+                }
+                else
+                {
+                    if (visited.Contains(temp))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        string newPath = temp.Path + "(" + temp.posX + "," + temp.posY + ") ";
+
+                        visited.Add(temp);
+                        myStack = temp.CallNeighbours(myStack, Grid, newPath);
+                    }
+                }
+                return false;
+
+            }
 
             void CreateGrid()
             {
@@ -55,10 +158,10 @@ namespace Walking_Dogs___Problem_Set
                 {
                     for (int xdir = 0; xdir < x * 4 + 1; xdir++)
                     {
-                        if ((xdir % 4 == 0 && ydir % 2 == 0))//|| (xdir == x * 4 && ydir % 2 == 0) || (ydir == y * 2 && xdir % 4 == 0) individual end calcs
+                        if ((xdir % 4 == 0 && ydir % 2 == 0))
                         {
                             Draw[ydir, xdir] = "+";
-                        }
+                        }//decides which string/class should be in each slot
                         else if (ydir % 2 != 0 && xdir % 4 == 0)
                         {
                             Draw[ydir, xdir] = "|";
@@ -67,19 +170,24 @@ namespace Walking_Dogs___Problem_Set
                         else if ((xdir % 4 != 0 && ydir % 2 == 0))
                         {
                             Draw[ydir, xdir] = "-";
-                        }                       
+                        }
                         else
                         {
                             if (count != 2)
                             {
                                 Draw[ydir, xdir] = " ";
                                 count++;
-                            }
+                            }//if not where the letter for cat would display, it is a space
                             else if (count == 2)
                             {
                                 count = 0;
 
                                 //finds if the slot is the Entry, the Exit or just a normal block
+
+                                //
+                                //xvalue is (xdir-2)/4, because each repeat is 4 long, 
+                                //but the "|" which only has 1 per square is -2 from the xdir's location. This will convert to the 0-based Grid index
+                                //
                                 if ((xdir - 2) / 4 == 0 && (ydir - 1) / 2 == 0)
                                 {
                                     Grid[0, 0] = new Entry(0, 0, x, y);
@@ -88,7 +196,7 @@ namespace Walking_Dogs___Problem_Set
                                 {
                                     Grid[y - 1, x - 1] = new Exit(x - 1, y - 1, x, y);
                                 }
-                                else
+                                else//if not an exit, or an entry, will be a normal square.
                                 {
                                     // will find if it is a cat, after has been defined as a normal square
 
@@ -103,26 +211,22 @@ namespace Walking_Dogs___Problem_Set
                 }
 
 
-            }  
-            
+            }
+
             bool SleepCat(int ydir, int xdir)
             {
-                
-
-               foreach(string[] s in CatLocations)
+                foreach (string[] s in CatLocations)
                 {
-                    if(int.Parse(s[0])-1 == (xdir - 2) / 4 && int.Parse(s[1])-1 == (ydir - 1) / 2)
+                    if (int.Parse(s[0]) - 1 == (xdir - 2) / 4 && int.Parse(s[1]) - 1 == (ydir - 1) / 2)
                     {
                         return true;
                     }
-                }
+                }//maintains the original Cat list, and rotates through each array of points
 
                 return false;
 
-            }
-            
-            //Creates the char grid    
-            
+            }//finds if an item is a cat. and sets it to one
+
             void Display()
             {
 
@@ -145,7 +249,8 @@ namespace Walking_Dogs___Problem_Set
                 }
 
 
-            }//displays the char grid
+            }//displays the grid in console.
+
         }
     }
 }
--- Walking Dogs - Problem Set/Square.cs
+++ Walking Dogs - Problem Set/Square.cs
@@ -10,37 +10,119 @@ namespace Walking_Dogs___Problem_Set
         public int posY;
         public int width;
         public int height;
-        public bool isCat { get; set; }        
+        public string Path;
+        public bool isCat { get; set; }
         protected string result;
         public Square(int posX, int posY, int width, int height, bool cat)
         {
             isCat = cat;
-            
+            Path = "";
             this.width = width;
             this.height = height;
             this.posX = posX;
             this.posY = posY;
             result = " ";
-        }       
+        }
 
 
-        public void Surrounding(Square surround)
+        public bool isNeighbour(Square sq, Square[,] Grid)
         {
+            try
+            {
+                if (Grid[posY - 1, posX] == sq) return true;
+            }
+            catch { }
+            try
+            {
+                if (Grid[posY + 1, posX] == sq) return true;
+            }
+            catch { }
+            try
+            {
+
+
+                if (Grid[posY, posX + 1] == sq) return true;
+            }
+            catch { }
+            try
+            {
+
+
+                if (Grid[posY, posX - 1] == sq) return true;
+            }
+            catch { }
+            return false;
+        }
+
+        public void MakeCat(int num, Square[,] Grid)
+        {            
+                isCat = true;
+                if (num == 0) return;
+                num -= 1;
+                MakeNeighbourCat(Grid, num);           
+        }
+
+        public Queue<Square> CallNeighbours(Queue<Square> myStack, Square[,] Grid, string path)
+        {
+            try
+            {
+                myStack.Enqueue(Grid[posY - 1, posX]);
+                Grid[posY - 1, posX].Path = path;
+            }
+            catch { }
+            try
+            {
+                myStack.Enqueue(Grid[posY + 1, posX]);
+                Grid[posY + 1, posX].Path = path;
+            }
+            catch { }
+            try
+            {
+                myStack.Enqueue(Grid[posY, posX + 1]);
+                Grid[posY, posX + 1].Path = path;
+            }
+            catch { }
+            try
+            {
+                myStack.Enqueue(Grid[posY, posX - 1]);
+                Grid[posY, posX - 1].Path = path;
+            }
+            catch { }
+
+            return myStack;
+        }
+
+        public void MakeNeighbourCat(Square[,] Grid, int num)
+        {
+            try
+            {
+                Grid[posY - 1, posX].MakeCat(num, Grid);
+            }
+            catch { }
+            try
+            {                               
+                Grid[posY + 1, posX].MakeCat(num, Grid);
+            }
+            catch { }
+            try
+            {                           
+                Grid[posY, posX + 1].MakeCat(num, Grid);
+            }
+            catch { }
+            try
+            {                            
+                Grid[posY, posX - 1].MakeCat(num, Grid);
+            }
+            catch { }
+
             
-
         }
-
-        public bool CheckCat()
-        {
-            return isCat;
-        }
-
 
         public override string ToString()
         {
             if (isCat)
             {
-               return "C";
+                return "C";
 
             }
             else
